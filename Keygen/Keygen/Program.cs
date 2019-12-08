142017cb69d668de0e4fb1bbd5ef8c01ef7f594c
Xeniya Denissova <Xeniya_Denissova@epam.com>
2019-12-08 20:22:49 +0600

    Program to crack + keygen

---
+++ Keygen/Keygen/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Text;
+
+namespace Keygen
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             byte[] addressBytes = NetworkInterface.GetAllNetworkInterfaces().FirstOrDefault().GetPhysicalAddress().GetAddressBytes();
+            byte[] dateBytes = BitConverter.GetBytes(DateTime.Now.Date.ToBinary());
+
+            var bytes = addressBytes.Select((a, index) => a ^ dateBytes[index]).Select(a => a * 10);
+
+            
+
+            Console.WriteLine(string.Join("-", bytes));
+        }
+    }
+}
